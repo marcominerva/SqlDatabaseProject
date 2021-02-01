bced16a3d890c71b0b7524512ab07cc194128810
Marco Minerva <marco.minerva@gmail.com>
2021-02-01 12:46:18 +0100

    Add sample column

--- SqlDatabaseProject/Tables/Products.sql
+++ SqlDatabaseProject/Tables/Products.sql
@@ -3,6 +3,7 @@
     [Name]        NVARCHAR (80)    NOT NULL,
     [Description] NVARCHAR (255)   NULL,
     [CategoryId] UNIQUEIDENTIFIER NULL, 
+    [Price] FLOAT NOT NULL DEFAULT 0, 
     PRIMARY KEY CLUSTERED ([Id] ASC), 
     CONSTRAINT [FK_Products_ToTable] FOREIGN KEY (CategoryId) REFERENCES Categories([Id])
 );
