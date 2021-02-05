ed566fc7f101186a7cd237c66e3a87bdac76ec27
Marco Minerva <marco.minerva@gmail.com>
2021-02-05 16:28:56 +0100

    Update Logs.sql

--- Scripts/Logs.sql
+++ Scripts/Logs.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE [dbo].[Logs](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Message] [nvarchar](max) NULL,
+	[Level] [nvarchar](128) NULL,
+	[Timestamp] [datetime2](7) NOT NULL,
+	[Exception] [nvarchar](max) NULL,
+	[LogEvent] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Logs] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)
+) 
